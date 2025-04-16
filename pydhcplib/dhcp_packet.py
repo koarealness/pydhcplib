a0caad810fcb0017b1885ca60e5e9fdaf35936fc
koarealness <64246827+koarealness@users.noreply.github.com>
2025-04-16 11:36:39 -0700

    Final Python 3 fixes for pydhcplib

--- pydhcplib/dhcp_packet.py
+++ pydhcplib/dhcp_packet.py
@@ -54,9 +54,7 @@ class DhcpPacket(DhcpBasicPacket):
                 hexsym = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
                 for iterator in range(6) :
                     result += [str(hexsym[data[iterator]/16]+hexsym[data[iterator]%16])]
-
                 result = ':'.join(result)
-
             printable_data += opt+" : "+result  + "\n"
 
         # Process options : 
@@ -143,7 +141,7 @@ class DhcpPacket(DhcpBasicPacket):
         elif p == 'chaddr' :
             try:
                 value = hwmac(value).list()+[0]*10
-           except (ValueError, TypeError):
+           except (ValueError,TypeError):
                 value = [0]*16
             return value
             
